b144a8490d51c0b18c97f54af974aa87c432d5d1
skodborg <skod92@gmail.com>
2015-09-15 17:40:32 +0200

    added log_cost impl to handin1

--- handin1/handin1.py
+++ handin1/handin1.py
@@ -7,8 +7,11 @@ datafile = np.load('mnistTrain.npz')
 
 print datafile.files
 
+# labels.shape = (60000,1)
 labels = datafile['labels']
+# images.shape = (60000,784)
 images = datafile['images']
+
 
 def visualize_image(imageidx):
 	image = images[imageidx]
@@ -23,9 +26,51 @@ def visualize_image(imageidx):
 	plt.pcolormesh(x, y, image)
 
 
-for i in range(0,10):
-	plt.figure(i)
-	visualize_image(i)
-	print str(i) + str(labels[i])
+# for i in range(0,10):
+# 	plt.figure(i)
+# 	visualize_image(i)
+# 	print str(i) + str(labels[i])
 
 # plt.show()
+
+
+def approximate_gradient(f, x, eps):
+	return (f(x+eps) - f(x-eps))/(2*eps)
+
+# print approximate_gradient(lambda x: x*x, 10, 0.0001)
+# prints 20.0 as expected
+
+
+def log_cost(X, Y, theta):
+	def logistic_func(z):
+		return 1 / (1 + np.exp(-z))
+
+	def weight_sum(theta, X):
+		return np.dot(np.transpose(theta), X)
+
+	def h(theta, X):
+		return logistic_func(weight_sum(theta, X))
+
+	fn_cost = lambda X, Y, theta: -np.sum([y * np.log(h(theta, x)) 
+		+ (1 - y) * np.log(1 - h(theta, x)) for x,y in zip(X,Y)])
+	gradient = np.dot(np.transpose(-X), (Y - logistic_func(np.dot(X, theta))))
+	return fn_cost, gradient
+
+
+X = np.array([[1,2,3],
+			  [1,2,3],
+			  [1,2,3],
+			  [1,2,3],
+			  [1,2,3],
+			  [1,2,3]])
+Y = np.array([7,3,9,0,4,2])
+theta = np.array([2,2,1])
+
+costfn, gradient = log_cost(X, Y, theta)
+# print type(costfn)
+# print type(gradient)
+# print costfn(X, Y, theta)
+# print gradient
+
+
+
