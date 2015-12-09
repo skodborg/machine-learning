905597e8ea4d68aec3e28afad8136ab7eae29e4c
Thahara <Thahara@users.noreply.github.com>
2015-12-09 13:46:15 +0100

    Implemented F-measures

--- clustering/f1.py
+++ clustering/f1.py
@@ -7,6 +7,10 @@ from __future__ import unicode_literals, division
 
 import numpy as np
 
+# faked test-data from book p. 428
+# fake_labels = np.append(np.append(np.append(np.append(np.ones(47), (np.ones(14)*2)), np.zeros(50)), np.ones(3)), (np.ones(36)*2)).astype('int')
+# fake_predic = np.append(np.append(np.append(np.append(np.zeros(47), np.zeros(14)), np.ones(50)), (np.ones(3)*2)), (np.ones(36)*2)).astype('int')
+
 
 def f1(predicted, labels):
     """
@@ -34,4 +38,22 @@ def f1(predicted, labels):
     r = np.max(predicted) + 1
     k = np.max(labels) + 1
 
-    TODO
+    # create and fill contingency table
+    contingency = np.zeros(r*k).reshape(r,k)
+    for i in range(0,n):
+        pred = predicted[i]
+        lbl = labels[i]
+        contingency[pred][lbl] += 1
+    
+    # calculate F-values
+    F_individual = []
+    for i in range(0,r):
+        n_i = list(predicted).count(i)
+        j_i = np.argmax(contingency[i])
+        n_iji = contingency[i][j_i]
+        m_ji = list(labels).count(j_i)
+        F_individual.append(2 * n_iji / (n_i + m_ji))
+        
+    F_overall = np.sum(F_individual)/r
+
+    return F_individual, F_overall, contingency
